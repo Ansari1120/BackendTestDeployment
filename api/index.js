9865a99e08e326666e7631b8bdfbc1b885b9a1e9
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-07-27 01:20:10 +0500

    "update 1.3"

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const courseRouter = require("./routes/courseRouter");
+const userRoutes = require("./routes/userRoutes");
+const StudentRouter = require("./routes/studentRouter");
+const TeacherRouter = require("./routes/teacherRouter");
+const InstituteRouter = require("./routes/instituteRouter");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/student", StudentRouter);
+app.use("/api/teacher", TeacherRouter);
+app.use("/api/institute", InstituteRouter);
+app.use("/api/course", courseRouter);
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server Started");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Database Connected Successfully and server is listening on this port 5000"
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
